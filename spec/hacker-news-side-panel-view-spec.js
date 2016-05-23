648ff05651b020fe4415429486770399f2bfc5d0
Monami Sharma <monami.sharma@gmail.com>
2016-05-23 16:44:26 -0500

    First commit

---
+++ spec/hacker-news-side-panel-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import HackerNewsSidePanelView from '../lib/hacker-news-side-panel-view';
+
+describe('HackerNewsSidePanelView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
